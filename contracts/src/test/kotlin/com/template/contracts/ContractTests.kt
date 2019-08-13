59ca36e9013953ffa472a6c34e6dd7d93f217ba1
Brenden Romanowski <brenden.romanowski@gmail.com>
2019-08-13 12:43:18 -0500

    Getting tokens workflow dependency working

--- contracts/src/test/kotlin/com/template/contracts/ContractTests.kt
+++ contracts/src/test/kotlin/com/template/contracts/ContractTests.kt
@@ -1,5 +1,11 @@
 package com.template.contracts
 
+import com.r3.corda.lib.tokens.contracts.states.NonFungibleToken
+import com.r3.corda.lib.tokens.contracts.types.IssuedTokenType
+import com.r3.corda.lib.tokens.contracts.types.TokenPointer
+import com.template.states.ExampleEvolvableToken
+import com.template.states.IOUToken
+import net.corda.core.contracts.LinearPointer
 import net.corda.testing.node.MockServices
 import org.junit.Test
 
@@ -8,6 +14,19 @@ class ContractTests {
 
     @Test
     fun `dummy test`() {
+//        val issuer = ledgerServices.identityService.getAllIdentities().first().party
+//        val recipient = ledgerServices.identityService.getAllIdentities().first().party
+////        val token = IOUToken("CUSTOMTOKEN", 2);
+////        val issuedFixedToken = IssuedTokenType(issuer, token);
+//
+//
+//        val token = ExampleEvolvableToken(listOf(), 2, "test");
+//        val linearPointer = LinearPointer(token.linearId, ExampleEvolvableToken::class.java);
+//        val tokenPointer = TokenPointer(linearPointer, token.fractionDigits);
+//        val issuedToken = IssuedTokenType(issuer, tokenPointer)
+
+
+
 
     }
 }
--- workflows/src/main/kotlin/com/template/flows/Flows.kt
+++ workflows/src/main/kotlin/com/template/flows/Flows.kt
@@ -1,8 +1,19 @@
 package com.template.flows
 
 import co.paralleluniverse.fibers.Suspendable
+import com.r3.corda.lib.tokens.contracts.states.NonFungibleToken
+import com.r3.corda.lib.tokens.contracts.types.IssuedTokenType
+import com.r3.corda.lib.tokens.contracts.types.TokenPointer
+import com.template.states.ExampleEvolvableToken
+import net.corda.core.contracts.LinearPointer
 import net.corda.core.flows.*
 import net.corda.core.utilities.ProgressTracker
+
+import com.r3.corda.lib.tokens.workflows.flows.issue.IssueTokensFlow
+import com.r3.corda.lib.tokens.workflows.flows.rpc.IssueTokens
+import net.corda.core.contracts.UniqueIdentifier
+import java.util.*
+import com.r3.corda.lib.tokens.contracts.utilities.getAttachmentIdForGenericParam
 
 // *********
 // * Flows *
@@ -14,7 +25,24 @@ class Initiator : FlowLogic<Unit>() {
 
     @Suspendable
     override fun call() {
-        // Initiator flow logic goes here.
+       //  Initiator flow logic goes here.
+//        val issuer = ledgerServices.identityService.getAllIdentities().first().party
+//        val recipient = ledgerServices.identityService.getAllIdentities().first().party
+////        val token = IOUToken("CUSTOMTOKEN", 2);
+//////        val issuedFixedToken = IssuedTokenType(issuer, token);
+////
+////
+//        val token = ExampleEvolvableToken(listOf(), 2, "test");
+//        val linearPointer = LinearPointer(token.linearId, ExampleEvolvableToken::class.java);
+//        val tokenPointer = TokenPointer(linearPointer, token.fractionDigits);
+//        val issuedToken = IssuedTokenType(issuer, tokenPointer)
+//
+//        val nonFungibleToken = NonFungibleToken(
+//                issuedToken, recipient,
+//                UniqueIdentifier.Companion.fromString(UUID.randomUUID().toString()),
+//                tokenPointer.getAttachmentIdForGenericParam()
+//        );
+//        subFlow(IssueTokens(listOf(nonFungibleToken)))
     }
 }
 
