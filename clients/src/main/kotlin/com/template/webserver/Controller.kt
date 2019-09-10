89cd998f6728ea21dda59c1be423eb259766fe5f
Brenden Romanowski <brenden.romanowski@gmail.com>
2019-09-10 14:09:55 -0500

    Vault Query exercises

--- clients/src/main/kotlin/com/template/webserver/Controller.kt
+++ clients/src/main/kotlin/com/template/webserver/Controller.kt
@@ -8,11 +8,13 @@ import com.template.states.IOUCustomSchema
 import com.template.states.IOUState
 import net.corda.core.contracts.Amount
 import net.corda.core.contracts.StateAndRef
+import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.identity.CordaX500Name
 import net.corda.core.messaging.CordaRPCOps
 import net.corda.core.messaging.startFlow
 import net.corda.core.messaging.startTrackedFlow
 import net.corda.core.messaging.vaultQueryBy
+import net.corda.core.node.services.Vault
 import net.corda.core.node.services.vault.Builder
 import net.corda.core.node.services.vault.CriteriaExpression
 import net.corda.core.node.services.vault.QueryCriteria
@@ -28,48 +30,30 @@ import java.util.*
 @RequestMapping("/") // The paths for HTTP requests are relative to this base path.
 class Controller(proxy: CordaRPCOps) {
 
-    init {
-        this.exerciseSetup()
-    }
-
     companion object {
         private val logger = LoggerFactory.getLogger(RestController::class.java)
     }
 
     private var proxy = proxy
 
-    @GetMapping(value = "/templateendpoint", produces = arrayOf("text/plain"))
-    public fun templateendpoint(): String {
-        return proxy.nodeInfo().legalIdentities[0].name.organisation
-        //return "Define an endpoint here."
-    }
-
-    @GetMapping(value = "/exerciseSetup")
-    public fun exerciseSetup(): Unit {
-       // proxy.startTrackedFlow(::IOUIssueFlow,
-//                IOUState(
-//                Amount(50, IOUToken("CUSTOMTOKEN", 2)),
-//                proxy.nodeInfo().legalIdentities[0], proxy.nodeInfo().legalIdentities[1])
-//        ).returnValue.get()
-//
-//        proxy.startTrackedFlow(::IOUIssueFlow,
-//                IOUState(
-//                Amount(80, IOUToken("CUSTOMTOKEN", 2)),
-//                proxy.nodeInfo().legalIdentities[0], proxy.nodeInfo().legalIdentities[1])
-//        ).returnValue.get()
-
-        print("\nOK")
-    }
-
-   @GetMapping(value = "/getIOUs")
+    @GetMapping(value = "/getIOUs")
     public fun getIOUs(): List<StateAndRef<IOUState>>? {
         //return null;
         return proxy.vaultQuery(IOUState::class.java).states
     }
 
+    @GetMapping(value = "/getIOUs/linearId/{linearId}")
+    public fun getIousWithLinearId(@PathVariable linearId: String): List<StateAndRef<IOUState>>? {
+        //return null;
+        val linearId = UniqueIdentifier.fromString(linearId)
+        val criteria = QueryCriteria.LinearStateQueryCriteria(
+                null, listOf(linearId));
+        return proxy.vaultQueryBy<IOUState>(criteria).states
+    }
 
-    @GetMapping(value = "/getIOUs/{amount}")
+    @GetMapping(value = "/getIOUs/greaterThan/{amount}")
     public fun getIOUsWithAmountGreaterThan(@PathVariable amount: Long): List<StateAndRef<IOUState>>? {
+        //return null;
         return builder {
             val criteria = QueryCriteria.VaultCustomQueryCriteria(
                     IOUCustomSchema.PersistentIOU::amount.greaterThan(amount)
--- clients/src/test/kotlin/com/template/webserver/ApiExercises.kt
+++ clients/src/test/kotlin/com/template/webserver/ApiExercises.kt
@@ -1,70 +1,94 @@
 package com.template.webserver
 
-import com.r3.corda.lib.tokens.money.FiatCurrency
-import com.template.contracts.ExchangeRateContract
-import com.template.contracts.IOUContract
-import com.template.flows.*
-import com.template.flows.ExchangeRateOracleFlow
+import com.template.flows.IOUIssueFlow
 import com.template.states.IOUState
 import com.template.states.IOUToken
-import com.template.states.IOUTokenState
 import net.corda.core.contracts.Amount
-import net.corda.core.contracts.Command
 import net.corda.core.identity.CordaX500Name
 import net.corda.core.messaging.startFlow
-import net.corda.core.messaging.vaultTrackBy
-import net.corda.core.node.ServiceHub
-import net.corda.core.node.services.Vault
-import net.corda.core.transactions.FilteredTransaction
-import net.corda.core.transactions.SignedTransaction
-import net.corda.core.transactions.TransactionBuilder
-import net.corda.core.transactions.WireTransaction
 import net.corda.core.utilities.getOrThrow
-import net.corda.testing.core.expect
-import net.corda.testing.core.expectEvents
+import net.corda.testing.core.TestIdentity
+import net.corda.testing.driver.DriverDSL
 import net.corda.testing.driver.DriverParameters
+import net.corda.testing.driver.NodeHandle
 import net.corda.testing.driver.driver
-import net.corda.testing.node.MockNetwork
-import net.corda.testing.node.MockNetworkParameters
 import net.corda.testing.node.TestCordapp
-import net.corda.testing.node.User
-import org.junit.After
-import org.junit.Before
 import org.junit.Test
-import rx.Observable
-import java.util.*
-import java.util.function.Predicate
+import java.util.concurrent.Future
 import kotlin.test.assertEquals
 
+
 class ApiExercises {
-    private val network = MockNetwork(MockNetworkParameters(cordappsForAllNodes = listOf(
-            TestCordapp.findCordapp("com.template.contracts"),
-            TestCordapp.findCordapp("com.template.flows")
-    )))
-    private val a = network.createNode()
-    private val b = network.createNode()
-    private val c = network.createNode(CordaX500Name("ExchangeRateOracleService", "New York", "US"))
+    private val bankA = TestIdentity(CordaX500Name("BankA", "", "GB"))
+    private val bankB = TestIdentity(CordaX500Name("BankB", "", "US"))
 
-    init {
-        listOf(a, b, c).forEach {
-            it.registerInitiatedFlow(IOUIssueFlowResponder::class.java)
-        }
+    @Test
+    fun `vault query`() = withDriver {
+        // Start a pair of nodes and wait for them both to be ready.
+        val (partyAHandle, partyBHandle) = startNodes(bankA, bankB)
+
+        val iou = IOUState(Amount(50, IOUToken("IOU_TOKEN", 2)),
+                partyAHandle.rpc.wellKnownPartyFromX500Name(bankA.name)!!, partyAHandle.rpc.wellKnownPartyFromX500Name(bankB.name)!!)
+
+        partyBHandle.rpc.startFlow(::IOUIssueFlow, iou).returnValue.getOrThrow()
+
+        val result = Controller(partyAHandle.rpc).getIOUs()!!
+        assertEquals(1, result.size)
+        assertEquals(50, result.get(0).state.data.amount.quantity)
     }
 
-    @Before
-    fun setup() = network.runNetwork()
+    @Test
+    fun `vault query linear id`() = withDriver {
+        // Start a pair of nodes and wait for them both to be ready.
+        val (partyAHandle, partyBHandle) = startNodes(bankA, bankB)
 
-    @After
-    fun tearDown() = network.stopNodes()
+        val iou = IOUState(Amount(50, IOUToken("IOU_TOKEN", 2)),
+                partyAHandle.rpc.wellKnownPartyFromX500Name(bankA.name)!!, partyAHandle.rpc.wellKnownPartyFromX500Name(bankB.name)!!)
+        val iou2 = IOUState(Amount(51, IOUToken("IOU_TOKEN", 2)),
+                partyAHandle.rpc.wellKnownPartyFromX500Name(bankA.name)!!, partyAHandle.rpc.wellKnownPartyFromX500Name(bankB.name)!!)
 
-    //@Test
-    fun testIouIssueFlow() {
-        val iou = IOUState(Amount(50, IOUToken("IOU_TOKEN", 2)), a.info.legalIdentities.get(0), b.info.legalIdentities.get(0))
+        partyBHandle.rpc.startFlow(::IOUIssueFlow, iou).returnValue.getOrThrow()
 
-        val future = a.startFlow(IOUIssueFlow(iou))
-        network.runNetwork()
-        val result = future.getOrThrow()
-
-        assertEquals(1, a.services.vaultService.queryBy(IOUState::class.java).states.size)
+        val result = Controller(partyAHandle.rpc).getIousWithLinearId(iou.linearId.toString())!!
+        assertEquals(1, result.size)
+        assertEquals(50, result.get(0).state.data.amount.quantity)
     }
+
+    @Test
+    fun `vault query custom schema`() = withDriver {
+        // Start a pair of nodes and wait for them both to be ready.
+        val (partyAHandle, partyBHandle) = startNodes(bankA, bankB)
+
+        partyBHandle.rpc.startFlow(::IOUIssueFlow,
+                IOUState(Amount(49, IOUToken("IOU_TOKEN", 2)),
+                partyAHandle.rpc.wellKnownPartyFromX500Name(bankA.name)!!, partyAHandle.rpc.wellKnownPartyFromX500Name(bankB.name)!!)
+        ).returnValue.getOrThrow()
+
+        partyBHandle.rpc.startFlow(::IOUIssueFlow,
+                IOUState(Amount(52, IOUToken("IOU_TOKEN", 2)),
+                        partyAHandle.rpc.wellKnownPartyFromX500Name(bankA.name)!!, partyAHandle.rpc.wellKnownPartyFromX500Name(bankB.name)!!)
+        ).returnValue.getOrThrow()
+
+        val result = Controller(partyAHandle.rpc).getIOUsWithAmountGreaterThan(50)!!
+        assertEquals(1, result.size)
+        assertEquals(52, result.get(0).state.data.amount.quantity)
+    }
+
+    // Runs a test inside the Driver DSL, which provides useful functions for starting nodes, etc.
+    private fun withDriver(test: DriverDSL.() -> Unit) = driver(
+        DriverParameters(isDebug = true, startNodesInProcess = true, cordappsForAllNodes = listOf(
+                TestCordapp.findCordapp("com.template.contracts"),
+                TestCordapp.findCordapp("com.template.flows")))
+    ) { test() }
+
+    // Makes an RPC call to retrieve another node's name from the network map.
+    private fun NodeHandle.resolveName(name: CordaX500Name) = rpc.wellKnownPartyFromX500Name(name)!!.name
+
+    // Resolves a list of futures to a list of the promised values.
+    private fun <T> List<Future<T>>.waitForAll(): List<T> = map { it.getOrThrow() }
+
+    // Starts multiple nodes simultaneously, then waits for them all to be ready.
+    private fun DriverDSL.startNodes(vararg identities: TestIdentity) = identities
+        .map { startNode(providedName = it.name) }
+        .waitForAll()
 }
